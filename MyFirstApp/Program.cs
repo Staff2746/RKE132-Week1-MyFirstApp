4e774696768061ff1f0a390686c1cdc87f7af084
Karl Kryynvald <karl.gustav.krnvald@tktk.ee>
2024-02-01 18:49:51 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+
+//Rakendus küsib kasutajalt sisestada oma nimi
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("enter your name:");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello" + ", "+ userName + "!");
+Console.WriteLine($"Hello, {userName}!");
+
+
+
